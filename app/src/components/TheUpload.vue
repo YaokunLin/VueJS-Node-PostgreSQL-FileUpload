1688c027422bb173a9e48f70d010d6122d0c4cc3
YaokunLin <yaokun1@ualberta.ca>
2023-08-21 12:29:54 -0700

    started a simple componet for FE

---
+++ app/src/components/TheUpload.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Enter Data</h2>
+    <p>
+      Please upload a CSV file from your local machine. Ensure that the CSV file includes the following columns:
+    </p>
+    <ul>
+      <li>Employee ID</li>
+      <li>Company</li>
+      <li>Industry</li>
+      <li>Work Hours</li>
+      <li>Vacation Hours</li>
+    </ul>
+    <input type="file" accept=".csv" @change="handleFileUpload" />
+    <button @click="submitCSV">Upload</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleFileUpload(event) {
+      // TODO: Implement the file handling logic here
+      console.log("File selected:", event.target.files[0]);
+    },
+    submitCSV() {
+      // TODO: Implement the API call to the backend to submit the CSV
+      console.log("Submitting CSV to the backend...");
+    },
+  },
+};
+</script>
