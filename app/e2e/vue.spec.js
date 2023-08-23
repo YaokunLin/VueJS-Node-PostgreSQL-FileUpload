20d8659908daebb07365f854952d22e9ccf728f1
YaokunLin <yaokun1@ualberta.ca>
2023-08-23 08:09:25 -0700

    updated playwright tests

--- app/e2e/vue.spec.js
+++ app/e2e/vue.spec.js
@@ -2,7 +2,8 @@ const { test, expect } = require('@playwright/test');
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
+test('should display the Reports component initially', async ({ page }) => {
+  await page.goto('http://localhost:3001');
+  const reportsComponent = await page.$('header');
+  expect(reportsComponent).not.toBeNull();
 })
